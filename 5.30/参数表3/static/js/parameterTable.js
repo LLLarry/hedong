9ae895b811e0c30460ce17c483d59ea7ddbc0b67
LLLarry <1079485906@qq,com>
2019-06-01 18:37:06 +0800

    6.1上传代码

--- 5.30/参数表3/static/js/parameterTable.js
+++ 5.30/参数表3/static/js/parameterTable.js
@@ -37,17 +37,51 @@ $(function(){
 						}
 						
 					})
+
 			if(flag){
 				console.log('通过验证')
 				// 这一步所有的input都已经通过验证了，然后需要获取是否显示电量的switch
-					var isBalanceRec= $("#isShowFirstPower").hasClass('mui-active') ? 1 : 0
+					var elecTimeFirst= $("#isShowFirstPower").hasClass('mui-active') ? 1 : 0    //是否初始显示电量 
 					/*发送ajax向后台传输数据,(获取每个input的数据)*/
-					var isBalanceRec= $("#isBalanceRec").hasClass('mui-active') ? 1 : 0  //获取按钮的数据
-					var isPowerFailure= $("#isPowerFailure").hasClass('mui-active') ? 1 : 0 //获取按钮的数据
+					var spRecMon= $("#isBalanceRec").hasClass('mui-active') ? 1 : 0  //是否回收
+					var spFullEmpty= $("#isPowerFailure").hasClass('mui-active') ? 1 : 0 //获取 是否断电自停
 					//这一步要获取所有的input 的数据进行上传数据操作
+					
+					var coinMin= $('input[name="coinMin"]').val().trim()
+					var cardMin= $('input[name="cardMin"]').val().trim()
+					var coinElec= $('input[name="coinElec"]').val().trim()
+					var cardElec= $('input[name="cardElec"]').val().trim()
+					var cst= $('input[name="cst"]').val().trim()
+					var fullPowerMin= $('input[name="fullPowerMin"]').val().trim()
+					var fullChargeTime= $('input[name="fullChargeTime"]').val().trim()
+					var powerMax1= $('input[name="powerMax1"]').val().trim()
+					var powerMax2= $('input[name="powerMax2"]').val().trim()
+					var powerMax3= $('input[name="powerMax3"]').val().trim()
+					var powerMax4= $('input[name="powerMax4"]').val().trim()
+					var powerTim2= $('input[name="powerTim2"]').val().trim()
+					var powerTim3= $('input[name="powerTim3"]').val().trim()
+					var powerTim4= $('input[name="powerTim4"]').val().trim()
 					$.ajax({
 						url: '#',
-						data: {},
+						data: {
+							elecTimeFirst,
+							spRecMon,
+							spFullEmpty,
+							coinMin,
+							cardMin,
+							coinElec,
+							cardElec,
+							cst,
+							fullPowerMin,
+							fullChargeTime,
+							powerMax1,
+							powerMax2,
+							powerMax3,
+							powerMax4,
+							powerTim2,
+							powerTim3,
+							powerTim4
+						},
 						success: function(e){
 							
 						}
